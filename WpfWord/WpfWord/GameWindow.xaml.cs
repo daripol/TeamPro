5a79673fc05033697f71c6aa3af873a7ec744560
Дарья Политова <ddpolitova@edu.hse.ru>
2019-12-20 23:40:09 +0300

    Добавила класс dame word manage

---
+++ WpfWord/WpfWord/GameWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfWord
+{
+    /// <summary>
+    /// Логика взаимодействия для GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private Game game;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            this.game = new Game();
+
+            UpdateAttempts();
+            UpdateCombo();
+            UpdateWord();
+
+            game.Attemts.OnValueChanged += UpdateAttempts;
+            game.Combo.OnValueChanged += UpdateCombo;
+            game.Word.OnValueChanged += UpdateWord;
+            game.OnGameEnded += TurnTryButtonOff;
+            game.OnGameEnded += () => MessageBox.Show("Игра закончена!");
+            game.OnGameStarted += TurnTryButtonOn;
+            game.OnGameStarted += () => log.Items.Clear();
+            game.OnNewGameEvent += LogEvent;
+        }
+
+        private void UpdateAttempts()
+        {
+            attempts.Content = game.Attemts.Value;
+        }
+
+        private void UpdateCombo()
+        {
+            combo.Content = game.Combo.Value;
+        }
+
+        private void UpdateWord()
+        {
+            word.Content = game.Word.Value;
+        }
+
+        private void TurnTryButtonOff()
+        {
+            tryButton.IsEnabled = false;
+        }
+
+        private void PrintEndMessage()
+        {
+            MessageBox.Show("Игра закончилась!");
+        }
+
+        private void TurnTryButtonOn()
+        {
+            tryButton.IsEnabled = true;
+        }
+
+        private void LogEvent(string eventInfo)
+        {
+            log.Items.Add(eventInfo);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            game.TryGuess(playerWord.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            game.Refresh();
+        }
+    }
+}
---
+++ WpfWord/WpfWord/GameWordManager.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WpfWord
+{
+    public class GameValue<T>
+    {
+        private T value;
+
+        public GameValue(T value)
+        {
+            this.value = value;
+        }
+
+        public event Action OnValueChanged;
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                OnValueChanged?.Invoke();
+            }
+        }
+    }
+
+    public class Game
+    {
+        public event Action OnGameStarted;
+        public event Action OnGameEnded;
+        public event Action<string> OnNewGameEvent;
+
+        private int a;
+
+        private GameValue<int> attempts;
+        private GameValue<int> combo;
+        private GameValue<string> computerWord;
+
+        public GameValue<int> Attemts => attempts;
+        public GameValue<int> Combo => combo;
+        public GameValue<string> Word => computerWord;
+
+        private List<string> words;
+        private List<string> usedWords = new List<string>();
+
+        public Game()
+        {
+            words = GetWordsList();
+            attempts = new GameValue<int>(5);
+            combo = new GameValue<int>(0);
+            computerWord = new GameValue<string>(GenerateWord());
+            OnGameStarted?.Invoke();
+        }
+
+        public void Refresh()
+        {
+            words = GetWordsList();
+            attempts.Value = 5;
+            combo.Value = 0;
+            computerWord.Value = GenerateWord();
+            OnGameStarted?.Invoke();
+            usedWords.Clear();
+        }
+
+        public void TryGuess(string playerWord)
+        {
+            if (!IsAvaliableWord(playerWord))
+            {
+                OnNewGameEvent?.Invoke($"Игрок ввел недопустимое слово \"{playerWord}\"");
+                return;
+            }
+            else
+            {
+                usedWords.Add(playerWord);
+            }
+
+            OnNewGameEvent?.Invoke($"Игрок ввел слово \"{playerWord}\"");
+
+            if (this.computerWord.Value.Length == playerWord.Length)
+            {
+                MoveEnded(true);
+                OnNewGameEvent?.Invoke("Вы угадали!");
+                computerWord.Value = GenerateWord();
+            }
+            else
+            {
+                MoveEnded(false);
+                OnNewGameEvent?.Invoke("Не угадали");
+            }
+            GameOverCheck();
+        }
+
+        private void MoveEnded(bool guessed)
+        {
+            if (guessed)
+            {
+                combo.Value++;
+                attempts.Value = 5;
+                computerWord.Value = GenerateWord();
+            }
+            else
+            {
+                combo.Value = 0;
+                attempts.Value--;
+            }
+        }
+
+        private void GameOverCheck()
+        {
+            if (attempts.Value == 0 || combo.Value == 3)
+                OnGameEnded?.Invoke();
+        }
+
+        private string GenerateWord()
+        {
+            int i = new Random().Next(0, words.Count);
+            string word = words[i];
+            if (IsAvaliableWord(word))
+            {
+                usedWords.Add(word);
+                return word;
+            }
+            else
+                return GenerateWord();
+        }
+
+        private bool IsAvaliableWord(string word)
+        {
+            return !usedWords.Contains(word) && words.Contains(word);
+        }
+
+        private List<string> GetWordsList()
+        {
+            List<string> tempList = new List<string>();
+
+            using (StreamReader sr = new StreamReader("word.txt", System.Text.Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    tempList.Add(line);
+                }
+            }
+
+            return tempList;
+        }
+    }
+}
